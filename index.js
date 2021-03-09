9a3cb6282e396db6418ab8ecf342189787f76a40
Otor John Stephen <johnogenyi4real@gmail.com>
2021-03-09 13:24:43 +0100

    added styles for mobile responsiveness and script function to handle password show and hide

--- index.js
+++ index.js
@@ -7,3 +7,14 @@ function openRegisterScreen() {
 function openLoginScreen() {
     card.style.transform= 'rotateY(0deg)';
 }
+
+var password = document.getElementById('password');
+var eye = document.getElementById('eye');
+
+eye.addEventListener('click', togglePassword);
+
+function togglePassword(){
+    eye.classList.toggle('active');
+
+    (password.type == 'password') ? password.type = 'text' : password.type = 'password';
+}
